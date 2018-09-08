610fa9c8b8b021eb3b97c3356ebdaa33168a2707
Paula <paulagarizcun@gmail.com>
2018-09-08 21:26:52 +0200

    created

--- recipes.js
+++ recipes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
-const data = require('./data.js')
+const data = require('./data.js');
+const Recipe = require('./models/Recipe')
 
 mongoose.connect('mongodb://localhost/recipeApp')
   .then(() => {
@@ -9,3 +10,13 @@ mongoose.connect('mongodb://localhost/recipeApp')
     console.error('Error connecting to mongo', err)
   });
 
+Recipe.create({
+  title: 'Trufas', 
+  level: 'Amateur Chef', 
+  ingredients: ['chocolate', 'harina', 'leche'],
+  cousine: 'Around the world', 
+  dishType: 'Dessert', 
+  duration: 30, 
+  creator: 'Cualquiera'}, (err, recipe) => {
+    (err) ? console.log(`Ha habido un error`, err) : console.log(`Se ha guardado una receta, ${recipe}`) 
+  });
